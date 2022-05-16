65299b1c4d65755c223cafb28307d66211ed0cb8
Yiming Shi <syiming@umich.edu>
2022-05-16 15:52:32 -0700

    random test - abort branch

--- escape-cube-main.js
+++ escape-cube-main.js
@@ -4,6 +4,99 @@ const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture
 } = tiny;
 
+// Default class to import shape from 3d file
+export class Shape_From_File extends Shape {
+                                    // **Shape_From_File** is a versatile standalone Shape that imports
+                                    // all its arrays' data from an .obj 3D model file.
+    constructor( filename )
+    { super( "position", "normal", "texture_coord" );
+        // Begin downloading the mesh. Once that completes, return
+        // control to our parse_into_mesh function.
+        this.load_file( filename );
+    }
+    load_file( filename )
+    {                             // Request the external file and wait for it to load.
+        // Failure mode:  Loads an empty shape.
+        return fetch( filename )
+            .then( response =>
+            { if ( response.ok )  return Promise.resolve( response.text() )
+            else                return Promise.reject ( response.status )
+            })
+            .then( obj_file_contents => this.parse_into_mesh( obj_file_contents ) )
+            .catch( error => { this.copy_onto_graphics_card( this.gl ); } )
+    }
+    parse_into_mesh( data )
+    {                           // Adapted from the "webgl-obj-loader.js" library found online:
+        var verts = [], vertNormals = [], textures = [], unpacked = {};
+
+        unpacked.verts = [];        unpacked.norms = [];    unpacked.textures = [];
+        unpacked.hashindices = {};  unpacked.indices = [];  unpacked.index = 0;
+
+        var lines = data.split('\n');
+
+        var VERTEX_RE = /^v\s/;    var NORMAL_RE = /^vn\s/;    var TEXTURE_RE = /^vt\s/;
+        var FACE_RE = /^f\s/;      var WHITESPACE_RE = /\s+/;
+
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i].trim();
+            var elements = line.split(WHITESPACE_RE);
+            elements.shift();
+
+            if      (VERTEX_RE.test(line))   verts.push.apply(verts, elements);
+            else if (NORMAL_RE.test(line))   vertNormals.push.apply(vertNormals, elements);
+            else if (TEXTURE_RE.test(line))  textures.push.apply(textures, elements);
+            else if (FACE_RE.test(line)) {
+                var quad = false;
+                for (var j = 0, eleLen = elements.length; j < eleLen; j++)
+                {
+                    if(j === 3 && !quad) {  j = 2;  quad = true;  }
+                    if(elements[j] in unpacked.hashindices)
+                        unpacked.indices.push(unpacked.hashindices[elements[j]]);
+                    else
+                    {
+                        var vertex = elements[ j ].split( '/' );
+
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 0]);
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 1]);
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 2]);
+
+                        if (textures.length)
+                        {   unpacked.textures.push(+textures[( (vertex[1] - 1)||vertex[0]) * 2 + 0]);
+                            unpacked.textures.push(+textures[( (vertex[1] - 1)||vertex[0]) * 2 + 1]);  }
+
+                        unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 0]);
+                        unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 1]);
+                        unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 2]);
+
+                        unpacked.hashindices[elements[j]] = unpacked.index;
+                        unpacked.indices.push(unpacked.index);
+                        unpacked.index += 1;
+                    }
+                    if(j === 3 && quad)   unpacked.indices.push( unpacked.hashindices[elements[0]]);
+                }
+            }
+        }
+        {
+            const { verts, norms, textures } = unpacked;
+            for( var j = 0; j < verts.length/3; j++ )
+            {
+                this.arrays.position     .push( vec3( verts[ 3*j ], verts[ 3*j + 1 ], verts[ 3*j + 2 ] ) );
+                this.arrays.normal       .push( vec3( norms[ 3*j ], norms[ 3*j + 1 ], norms[ 3*j + 2 ] ) );
+                this.arrays.texture_coord.push( vec( textures[ 2*j ], textures[ 2*j + 1 ] ) );
+            }
+            this.indices = unpacked.indices;
+        }
+        this.normalize_positions( false );
+        this.ready = true;
+    }
+    draw( context, program_state, model_transform, material )
+    {               // draw(): Same as always for shapes, but cancel all
+        // attempts to draw the shape before it loads:
+        if( this.ready )
+            super.draw( context, program_state, model_transform, material );
+    }
+}
+
 export class EscapeCubeMain extends Scene {
     constructor() {
         super();
@@ -11,7 +104,9 @@ export class EscapeCubeMain extends Scene {
         this.shapes = {
             torus: new defs.Torus(15, 15),
             wall: new defs.Cube(),
-            light: new defs.Subdivision_Sphere(4),
+            light: new defs.Cube(),
+            fire: new Shape_From_File("assets/fire.obj"),
+            torch: new Shape_From_File("assets/Torch/Torch.obj")
         };
         const bump = new defs.Fake_Bump_Map(1);
 
@@ -28,10 +123,20 @@ export class EscapeCubeMain extends Scene {
                 ambient: 0.2, diffusivity: 1, specularity: 0.9,
                 texture: new Texture("assets/brick-wall.jpeg")
             }),
-            light: new Material(new defs.Phong_Shader(), {
+            // light: new Material(bump, {
+            //     ambient: 0.8, diffusivity: 0, specularity: 0,
+            //     color: hex_color("#000000"),
+            //     texture: new Texture("assets/flame.png"),
+            // }),
+            // fire: new Material(new defs.Textured_Phong(1), {
+            //     ambient: 0.8, diffusivity: 0, specularity: 0,
+            //     color: hex_color("#000000"),
+            //     texture: new Texture("assets/flame.png"),
+            // }),
+            torch: new Material(new defs.Textured_Phong(1), {
                 ambient: 0.8, diffusivity: 0, specularity: 0,
-                color: hex_color("#B5672D"),
-            }),
+                color: hex_color("#000000"),
+                texture: new Texture("assets/Torch/VRayMtl1SG_Base_Color copy.jpg"),}),
         };
 
         this.initial_camera_location = Mat4.look_at(vec3(0, 0, 12), vec3(0, 0, 0), vec3(0, 1, 1));
@@ -108,11 +213,14 @@ export class EscapeCubeMain extends Scene {
         model_transform = Mat4.identity()
             .times(Mat4.translation(7, 4, 2));
 
-        this.shapes.light.draw(context, program_state, model_transform, this.materials.light.override({color: color(1, redness, 0, 1), ambient: redness}));
+        // let light_model_transform = model_transform.times(Mat4.scale(0.2,0.2,0.2));
+        // this.shapes.light.draw(context, program_state, light_model_transform, this.materials.light);
 
-        model_transform = Mat4.identity()
-            .times(Mat4.translation(-7, 4, 2));
+        // model_transform = Mat4.identity()
+        //     .times(Mat4.translation(-7, 4, 2));
+        // this.shapes.fire.draw(context, program_state, model_transform, this.materials.fire);
 
-        this.shapes.light.draw(context, program_state, model_transform, this.materials.light.override({color: color(1, redness, 0, 1), ambient: redness}));
+        let head_model_transform = Mat4.identity();
+        this.shapes.torch.draw(context, program_state, head_model_transform, this.materials.torch);
     }
 }
