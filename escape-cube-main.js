895538015a50dabaa1368dec3268baed9c9281d0
Yiming Shi <syiming@umich.edu>
2022-05-18 12:30:33 -0700

    constuct larger arena

--- escape-cube-main.js
+++ escape-cube-main.js
@@ -13,25 +13,29 @@ export class EscapeCubeMain extends Scene {
         this.shapes = {
             torus: new defs.Torus(15, 15),
             wall: new defs.Cube(),
+            floor: new defs.Cube(),
+            arena_wall: new defs.Cube(),
             light: new defs.Subdivision_Sphere(3),
             gun: new Shape_From_File("assets/gun.obj"),
             bullet: new Shape_From_File("assets/45.obj"),
             lantern: new Shape_From_File("assets/sconce.obj"),
             monster: new Shape_From_File("assets/skull.obj"),
         };
-        const bump = new defs.Fake_Bump_Map(1);
+        const bump = new defs.Fake_Bump_Map(2);
+        this.shapes.floor.arrays.texture_coord.forEach(p => p.scale_by(120));
+        this.shapes.arena_wall.arrays.texture_coord.forEach(p => p.scale_by(8));
 
         this.materials = {
             test: new Material(new defs.Phong_Shader(),
                 {ambient: 1, diffusivity: .4, color: hex_color("#412c18")}),
             wall: new Material(bump, {
                 color: hex_color("#000000"),
-                ambient: 0.2, diffusivity: 1, specularity: 0.9,
-                texture: new Texture("assets/brick-wall.jpeg")
+                ambient: 0.4, diffusivity: 1, specularity: 0.9,
+                texture: new Texture("assets/brick-wall.jpeg", "LINEAR_MIPMAP_LINEAR")
             }),
             floor: new Material(bump, {
                 color: hex_color("#000000"),
-                ambient: 0.3, diffusivity: 1, specularity: 0.9,
+                ambient: 0.5, diffusivity: 1, specularity: 0.9,
                 texture: new Texture("assets/floor.jpeg")
             }),
             light: new Material(new defs.Phong_Shader(), {
@@ -60,7 +64,7 @@ export class EscapeCubeMain extends Scene {
             }),
             monster: new Material(new defs.Phong_Shader(), {
                 color: hex_color("#720F14"),
-                ambient: 0.3, diffusivity: 0.4, specularity: 0.2
+                ambient: 0.3, diffusivity: 0.4, specularity: 0.4
             })
 
         };
@@ -129,12 +133,12 @@ export class EscapeCubeMain extends Scene {
         },undefined, () => {this.update = false;});
 
         this.key_triggered_button("rotate left", ["q"], () => {
-            this.current_camera_location = Mat4.rotation(-0.2, 0, 1, 0).times(this.current_camera_location);
+            this.current_camera_location = Mat4.rotation(-0.4, 0, 1, 0).times(this.current_camera_location);
             this.update = true;
         },undefined, () => {this.update = false;});
 
         this.key_triggered_button("rotate right", ["e"], () => {
-            this.current_camera_location = Mat4.rotation(0.2, 0, 1, 0).times(this.current_camera_location);
+            this.current_camera_location = Mat4.rotation(0.4, 0, 1, 0).times(this.current_camera_location);
             this.update = true;
         },undefined, () => {this.update = false;});
 
@@ -159,69 +163,84 @@ export class EscapeCubeMain extends Scene {
             Math.PI / 4, context.width / context.height, .1, 1000);
 
         const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
-        let redness_1 = 1 + 0.1*Math.sin(3*t) + 0.2*Math.cos(7*t);
-        let redness_2 = 1.2 + 0.15*Math.sin(5*t) + 0.2*Math.cos(8.5*t);
+
+        // ************************************************************************************************
+        // common
+        // ************************************************************************************************
+
+        // lights
+        let redness_1 = 1 + 0.1*Math.sin(3*t) + 0.4*Math.cos(7*t);
+        let redness_2 = 1.2 + 0.15*Math.sin(5*t) + 0.4*Math.cos(8.5*t);
         // The parameters of the Light are: position, color, size
         program_state.lights = [
             new Light(vec4(-13.5, 4.5, -8, 1), color(1, redness_1, 0, 1), 40*redness_1)
         ];
+
+
+        // ************************************************************************************************
+        // entry room
+        // ************************************************************************************************
+
+        // walls
         let model_transform = Mat4.identity()
             .times(Mat4.translation(-15, 0 ,0))
-            .times(Mat4.scale(0.2, 8, 15));
-
+            .times(Mat4.scale(0.4, 8, 15));
         this.shapes.wall.draw(context, program_state, model_transform, this.materials.wall);
-        model_transform = Mat4.identity()
-            .times(Mat4.translation(-14, 4, -8))
-            .times(Mat4.rotation(0.5*Math.PI, 0, 1, 0))
-            .times(Mat4.rotation(-0.5*Math.PI, 1, 0, 0));
 
-        this.shapes.lantern.draw(context, program_state, model_transform, this.materials.test);
         model_transform = Mat4.identity()
-            .times(Mat4.translation(-13.5, 5, -8))
-            .times(Mat4.scale(0.1, 0.5*redness_1, 0.1));
-        this.shapes.light.draw(context, program_state, model_transform, this.materials.light.override({color: color(1, redness_1, 0, 1), ambient:redness_1}));
+            .times(Mat4.translation(15, 0 ,0))
+            .times(Mat4.scale(0.4, 8, 15));
+        this.shapes.wall.draw(context, program_state, model_transform, this.materials.wall);
+
+        model_transform = Mat4.identity()
+            .times(Mat4.translation(0, 0 ,15))
+            .times(Mat4.scale(15, 8, 0.4));
+        this.shapes.wall.draw(context, program_state, model_transform, this.materials.wall);
+
+        model_transform = Mat4.identity()
+            .times(Mat4.translation(0, 8 ,0))
+            .times(Mat4.scale(15, 0.4, 15));
+        this.shapes.wall.draw(context, program_state, model_transform, this.materials.ceiling);
 
         let front_wall = Mat4.identity()
             .times(Mat4.translation(-17, 0 ,-15))
             .times(Mat4.scale(12, 8, 0.5));
         this.shapes.wall.draw(context, program_state, front_wall, this.materials.wall);
 
+        // lights
+        // model_transform = Mat4.identity()
+        //     .times(Mat4.translation(-14, 4, -8))
+        //     .times(Mat4.rotation(0.5*Math.PI, 0, 1, 0))
+        //     .times(Mat4.rotation(-0.5*Math.PI, 1, 0, 0));
+        // this.shapes.lantern.draw(context, program_state, model_transform, this.materials.test);
+
+
+        model_transform = Mat4.identity()
+            .times(Mat4.translation(-13.5, 5, -8))
+            .times(Mat4.scale(0.1, 0.5*redness_1, 0.1));
+        this.shapes.light.draw(context, program_state, model_transform, this.materials.light.override({color: color(1, redness_1, 0, 1), ambient:redness_1}));
+
+        // model_transform = Mat4.identity()
+        //     .times(Mat4.translation(14, 4, -8))
+        //     .times(Mat4.rotation(-0.5*Math.PI, 0, 1, 0))
+        //     .times(Mat4.rotation(-0.5*Math.PI, 1, 0, 0));
+        // this.shapes.lantern.draw(context, program_state, model_transform, this.materials.test);
+
         program_state.lights = [
             new Light(vec4(13.5, 4.5, -8, 1), color(1, redness_2, 0, 1), 30)
         ];
-        model_transform = Mat4.identity()
-            .times(Mat4.translation(15, 0 ,0))
-            .times(Mat4.scale(0.2, 8, 15));
-        this.shapes.wall.draw(context, program_state, model_transform, this.materials.wall);
-
-        model_transform = Mat4.identity()
-            .times(Mat4.translation(0, 0 ,15))
-            .times(Mat4.scale(15, 8, 0.2));
-        this.shapes.wall.draw(context, program_state, model_transform, this.materials.wall);
-
-        model_transform = Mat4.identity()
-            .times(Mat4.translation(0, -8 ,0))
-            .times(Mat4.scale(15, 0.2, 15));
-        this.shapes.wall.draw(context, program_state, model_transform, this.materials.floor);
-
-        model_transform = Mat4.identity()
-            .times(Mat4.translation(0, 8 ,0))
-            .times(Mat4.scale(15, 0.2, 15));
-        this.shapes.wall.draw(context, program_state, model_transform, this.materials.ceiling);
-
-        model_transform = Mat4.identity()
-            .times(Mat4.translation(14, 4, -8))
-            .times(Mat4.rotation(-0.5*Math.PI, 0, 1, 0))
-            .times(Mat4.rotation(-0.5*Math.PI, 1, 0, 0));
-
-        this.shapes.lantern.draw(context, program_state, model_transform, this.materials.test);
-
         model_transform = Mat4.identity()
             .times(Mat4.translation(13.5, 5, -8))
             .times(Mat4.scale(0.1, 0.5*redness_2, 0.1));
         this.shapes.light.draw(context, program_state, model_transform, this.materials.light.override({color: color(1, redness_2, 0, 1), ambient:redness_2}));
 
-        //this.shapes.lantern.draw(context, program_state, Mat4.identity(), this.materials.test);
+        // draw floor of entire scene
+        const floor_size = 1000;
+        model_transform = Mat4.identity()
+            .times(Mat4.translation(0, -8 ,0))
+            .times(Mat4.scale(floor_size, 0.4, floor_size));
+
+        this.shapes.floor.draw(context, program_state, model_transform, this.materials.floor);
 
         let eye_loc = program_state.camera_inverse.times(vec4(0,0,0,1));
 
@@ -264,7 +283,7 @@ export class EscapeCubeMain extends Scene {
                 .times(Mat4.inverse(program_state.camera_inverse))
                 .times(Mat4.translation(1.85,-1-((this.bullet_loc[i]/1000.0)**2)*0.5*9.8*2,-10.5-this.bullet_loc[i]))
                 .times(Mat4.rotation(0.5*Math.PI, 1, 0, 0))
-                .times(Mat4.scale(0.15, 0.15, 0.2));
+                .times(Mat4.scale(0.15, 0.15, 0.4));
             this.shapes.bullet.draw(context, program_state, bullet, this.materials.bullet);
         }
         let i = 0;
@@ -274,45 +293,64 @@ export class EscapeCubeMain extends Scene {
         }
 
 
-        //main arena
+        // ************************************************************************************************
+        // main arenas
+        // ************************************************************************************************
         program_state.lights = [
             new Light(vec4(13.5, 10, -16, 1), color(1, 1, 1, 1), 1000)
         ];
-        let floor_main = Mat4.identity()
-            .times(Mat4.translation(-15, -8 ,-30))
-            .times(Mat4.scale(15, 0.2, 15));
-        this.shapes.wall.draw(context, program_state, floor_main, this.materials.floor);
-        floor_main = Mat4.translation(30, 0, 0).times(floor_main);
-        this.shapes.wall.draw(context, program_state, floor_main, this.materials.floor);
-        floor_main = Mat4.translation(0, 0, -30).times(floor_main);
-        this.shapes.wall.draw(context, program_state, floor_main, this.materials.floor);
-        floor_main = Mat4.translation(-30, 0, 0).times(floor_main);
-        this.shapes.wall.draw(context, program_state, floor_main, this.materials.floor);
 
-        let side_wall = Mat4.identity()
-            .times(Mat4.translation(-30, 0 ,-30))
-            .times(Mat4.scale(0.2, 8, 15));
+        const arena_size = 60;
+        const arena_height = 80;
+        let arena_wall = Mat4.identity()
+            .times(Mat4.translation(-arena_size*2,  -15, -arena_size*2 - 15))
+            .times(Mat4.scale(0.4, arena_height, arena_size*2))
+        this.shapes.arena_wall.draw(context, program_state, arena_wall, this.materials.wall);
 
-        this.shapes.wall.draw(context, program_state, side_wall, this.materials.wall);
-        side_wall = Mat4.translation(0,0,-30).times(side_wall);
-        this.shapes.wall.draw(context, program_state, side_wall, this.materials.wall);
-        side_wall = Mat4.translation(60,0,0).times(side_wall);
-        this.shapes.wall.draw(context, program_state, side_wall, this.materials.wall);
-        side_wall = Mat4.translation(0,0,30).times(side_wall);
-        this.shapes.wall.draw(context, program_state, side_wall, this.materials.wall);
-        side_wall = Mat4.identity()
-            .times(Mat4.translation(-15, 0 ,-60))
-            .times(Mat4.scale(15, 8, 0.2));
-        this.shapes.wall.draw(context, program_state, side_wall, this.materials.wall);
-        side_wall = Mat4.translation(30,0,0).times(side_wall);
-        this.shapes.wall.draw(context, program_state, side_wall, this.materials.wall);
+        arena_wall = Mat4.identity()
+            .times(Mat4.translation(-arena_size*2-5,  -15,-15.8))
+            .times(Mat4.scale(arena_size*2, arena_height, 0.8));
+        this.shapes.arena_wall.draw(context, program_state, arena_wall, this.materials.wall);
 
-        let monster_trans = Mat4.identity()
-            .times(Mat4.translation(-15, 0 ,-50))
-            .times(Mat4.rotation(t, 0, 1, 0))
-            .times(Mat4.rotation(-0.5*Math.PI, 1, 0, 0))
-            .times(Mat4.scale(2,2,2));
-        this.shapes.monster.draw(context, program_state, monster_trans, this.materials.monster);
+        arena_wall = Mat4.identity()
+            .times(Mat4.translation(arena_size*2+5,  -15,-15.8))
+            .times(Mat4.scale(arena_size*2, arena_height, 0.8))
+        this.shapes.arena_wall.draw(context, program_state, arena_wall, this.materials.wall);
+
+        arena_wall = Mat4.identity()
+            .times(Mat4.translation(0,  8 + arena_height,-15.8))
+            .times(Mat4.scale(arena_size*2, arena_height, 0.81))
+        this.shapes.arena_wall.draw(context, program_state, arena_wall, this.materials.wall);
+
+        arena_wall = Mat4.identity()
+            .times(Mat4.translation(arena_size*2,  -15, -arena_size*2 - 15))
+            .times(Mat4.scale(0.4, arena_height, arena_size*2))
+        this.shapes.arena_wall.draw(context, program_state, arena_wall, this.materials.wall);
+
+        arena_wall = Mat4.identity()
+            .times(Mat4.translation(0,  -15,-15.8 - 4 * arena_size))
+            .times(Mat4.scale(arena_size*2, arena_height, 0.4))
+        this.shapes.arena_wall.draw(context, program_state, arena_wall, this.materials.wall);
+
+        // side_wall = Mat4.translation(0,0,-30).times(side_wall);
+        // this.shapes.wall.draw(context, program_state, side_wall, this.materials.wall);
+        // side_wall = Mat4.translation(60,0,0).times(side_wall);
+        // this.shapes.wall.draw(context, program_state, side_wall, this.materials.wall);
+        // side_wall = Mat4.translation(0,0,30).times(side_wall);
+        // this.shapes.wall.draw(context, program_state, side_wall, this.materials.wall);
+        // side_wall = Mat4.identity()
+        //     .times(Mat4.translation(-15, 0 ,-60))
+        //     .times(Mat4.scale(15, 8, 0.4));
+        // this.shapes.wall.draw(context, program_state, side_wall, this.materials.wall);
+        // side_wall = Mat4.translation(30,0,0).times(side_wall);
+        // this.shapes.wall.draw(context, program_state, side_wall, this.materials.wall);
+
+        // let monster_trans = Mat4.identity()
+        //     .times(Mat4.translation(-15, 0 ,-50))
+        //     .times(Mat4.rotation(t, 0, 1, 0))
+        //     .times(Mat4.rotation(-0.5*Math.PI, 1, 0, 0))
+        //     .times(Mat4.scale(2,2,2));
+        // this.shapes.monster.draw(context, program_state, monster_trans, this.materials.monster);
 
 
 
